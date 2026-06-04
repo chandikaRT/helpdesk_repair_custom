28eacbf9213192b29a1171cd7b4bfe38136de4f6
Chandika R <chandika.r@jinasena.com.lk>
2026-06-04 09:34:22 +0000

    [claude] S00057 REPAIR/2026/02069 in this ticket, in the task when i click mark as done t | 1 file(s) changed

--- models/sale_order_line.py
+++ models/sale_order_line.py
@@ -12,14 +12,14 @@ class SaleOrderLine(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        lines = super().create(vals_list)
-        for line in lines:
-            if (line.order_id.x_studio_rug_confirmed
-                    and line.order_id.x_studio_is_repair_order
-                    and not line.x_studio_price_unit_original
-                    and line.product_id):
-                line.write({
-                    'x_studio_price_unit_original': line.price_unit,
-                    'price_unit': line.product_id.standard_price,
-                })
-        return lines
+        for vals in vals_list:
+            order_id = vals.get('order_id')
+            product_id = vals.get('product_id')
+            if not order_id or not product_id or vals.get('x_studio_price_unit_original'):
+                continue
+            order = self.env['sale.order'].browse(order_id)
+            if order.x_studio_rug_confirmed and order.x_studio_is_repair_order:
+                product = self.env['product.product'].browse(product_id)
+                vals['x_studio_price_unit_original'] = vals.get('price_unit', product.lst_price)
+                vals['price_unit'] = product.standard_price
+        return super().create(vals_list)
